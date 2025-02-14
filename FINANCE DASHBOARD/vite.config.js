2bceca1b4e28e46ebd5c4ac64349674f87f927a5
Mohamed-Makram47 <mohamedmakram2323@gmail.com>
2025-02-14 02:35:50 +0200

    Build and deploy

--- FINANCE DASHBOARD/vite.config.js
+++ FINANCE DASHBOARD/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/FINANCE-DASHBOARD/', // Replace 'FINANCE-DASHBOARD' with your repository name
+  build: {
+    outDir: 'dist', // Optional: ensure the build output folder is set
+  },
+});
